718673cf8c95be03b04213338d1d8b3c03ee9dd8
demonkiller <gaurangvishwakarma619@gmail.com>
2021-08-28 21:36:23 +0530

    added bspwm files

---
+++ profiledef.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="archlinux-gui-bspwm"
+iso_label="ARCH_BSPWM_$(date +%Y%m)"
+iso_publisher="DemonKiller"
+iso_application="Arch Linux GUI Live/Rescue CD"
+iso_version="$(date +%Y.%m)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/etc/skel/.config/bspwm/bspwmrc"]="0:0:755"
+  ["/etc/skel/.config/sxhkd/sxhkdrc"]="0:0:644"
+  ["/etc/skel/.config/polybar/launch.sh"]="0:0:755"
+)
